78cc4b7d5dfc7ba62f2cad3d71a4079398f24bb3
alexandra-birsan <birsan.dev@gmail.com>
2021-09-16 09:12:53 +0300

    Magnet pattern

--- src/main/scala/lectures/part4implicits/MagnetPattern.scala
+++ src/main/scala/lectures/part4implicits/MagnetPattern.scala
@@ -1,6 +1,6 @@
 package lectures.part4implicits
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 object MagnetPattern extends App {
 
@@ -27,7 +27,88 @@ object MagnetPattern extends App {
      */
   }
 
-  trait MessageMagnet[Result]
+  trait MessageMagnet[Result]{
 
+    def apply():Result
+  }
+
+  def receive[R](magnet:MessageMagnet[R]):R =  magnet()
+
+  implicit class FromPsPRequest(request:P2PRequest) extends MessageMagnet[Int]{
+
+    override def apply(): Int = {
+      // logic for handling a P2P request
+      println("Handling P2P request")
+      42
+    }
+  }
+
+  implicit class FromP2PResponse(response: P2PResponse) extends MessageMagnet[Int] {
+    override def apply(): Int = {
+      // logic for handling a PSPResponse
+      println("Handling P2P response")
+      24
+    }
+  }
+
+  receive(new P2PRequest)
+  receive(new P2PResponse) // an implicit conversion is made from a P2PResponse to a message magnet, which will then be sent to the
+  // receive method, which will then call the apply method, which has our handling logic
+
+  /*
+  MAGNET PATTERN
+  benefits:
+  - no more type erasure problems!
+   */
+
+  implicit class FromResponseFuture(future: Future[P2PResponse]) extends MessageMagnet[Int] {
+    override def apply(): Int = 2
+  }
+
+  implicit class FromRequestFuture(future: Future[P2PRequest]) extends MessageMagnet[Int]{
+
+    override def apply(): Int = 4
+  }
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  println(receive(Future(new P2PRequest)))
+  println(receive(Future(new P2PResponse)))
+
+  // 2 - lifting works
+  trait MathLib {
+
+    def add1(x:Int): Int = x+ 1
+    def add1(s:String): Int = s.toInt + 1
+    // add1 overloads
+  }
+
+  // "magnetize": magnet trait, magnet method, implicit conversions
+  trait AddMagnet { // OBS: it has not type param here
+    def apply(): Int
+  }
+
+  def add1(magnet:AddMagnet):Int = magnet() // a magnet method
+
+  implicit class AddInt(x:Int) extends AddMagnet {
+    override def apply(): Int = x +1
+  }
+
+  implicit class AddString(s:String) extends AddMagnet {
+    override def apply(): Int = s.toInt + 1
+  }
+
+  val addFV = add1 _
+  println(addFV(1)) // we can lift our magnet function for use in HOF. It works because we didn't put the type param to the AddMagnet trait.
+  // The compiler would not know what it is
+  println(addFV("3"))
+
+  /*
+  Drawbacks:
+  - super verbose
+  2 - the API is harder to read
+  3 - you can't name or place default args (receive() ?)
+  4 - call by name does not work correctly (hint: side effects)
+   */
 }
 
